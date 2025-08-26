6edc34bafe4aa3c4b3027b9879ce9d572633f831
Robert (Bobby) DeLucia <bdelucia@asu.edu>
2025-08-26 12:52:19 -0700

    trying to get mdx components to render

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from "mdx/types";
+import BlogCarousel from "./components/BlogCarousel";
 
-const components: MDXComponents = {};
+const components: MDXComponents = { BlogCarousel };
 
 export function useMDXComponents(): MDXComponents {
     return components;
