91b6ee5eb957e1114691ae9e335b78f0c2589656
Robert (Bobby) DeLucia <bdelucia@asu.edu>
2025-09-11 19:43:13 -0700

    fix: ripping the integrated blog out

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configure `pageExtensions` to include markdown and MDX files
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
     // Configure images to allow external domains
     images: {
         remotePatterns: [
@@ -26,19 +21,6 @@ const nextConfig = {
     // Performance optimizations
     compress: true,
     poweredByHeader: false,
-    // Optionally, add any other Next.js config below
-    experimental: {
-        mdxRs: true,
-    },
 };
 
-const withMDX = createMDX({
-    options: {
-        remarkPlugins: [remarkGfm],
-    },
-
-    extension: /\.(md|mdx)$/,
-});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
